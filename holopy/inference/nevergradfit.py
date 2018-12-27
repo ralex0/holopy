d8e048d69078b3558e2dd97bc930684f0042d849
ralex0 <ralex0@users.noreply.github.com>
2018-12-26 23:22:22 -0500

    test for fitting a hologram with nevergrad

--- holopy/inference/nevergradfit.py
+++ holopy/inference/nevergradfit.py
@@ -6,8 +6,13 @@ import time
 import numpy as np
 
 from holopy.core.holopy_object import HoloPyObject
-from holopy.inference.result import InferenceResult, UncertainValue
+from holopy.inference.model import AlphaModel
+from holopy.inference.result import FitResult, InferenceResult, UncertainValue
+from holopy.scattering import calc_holo
+from holopy.scattering.scatterer import Sphere
+from holopy.scattering.errors import InvalidScatterer
 
+from nevergrad import instrumentation as instru
 from nevergrad.optimization import optimizerlib
 
 class NevergradStrategy(HoloPyObject):
@@ -17,22 +22,56 @@ class NevergradStrategy(HoloPyObject):
 
     def optimize(self, model, data):
         time_start = time.time()
-        cost = self._make_cost_function(model, data)
+        cost = self.cost_function(model, data)
         dim = self._get_model_diminsion(model)
         optimizer = self._setup_optimizer(dim)
         reccomendation = optimizer.optimize(cost, executor=None, batch_mode=True)
         parameters = {key: value for key, value in zip(model.parameters.keys(), reccomendation)}
-
         perrors = self._estimate_error_from_fit()
         intervals = [UncertainValue(value, perrors, name=key)
                      for key, value in parameters.items()]
 
         d_time = time.time() - time_start
-        result = InferenceResult(data=data, model=model, strategy=optimizer, intervals=intervals, time=d_time)
+        best_scatterer, best_scaling = self._scatterer_from_optimizer_params(reccomendation)
+        result = FitResult(data=data, model=model, strategy=optimizer, 
+                           intervals=intervals, time=d_time, kwargs={})
         return result
 
+    def cost_function(self, model, data):
+        if hasattr(model, 'theory'):
+            def cost(*x):
+                params, sixth = self._scatterer_from_optimizer_params(x)
+                optics, scatterer = model._optics_scatterer(params, data)
+                return self._cost_function(data, scatterer, alpha=sixth)
+                #return np.min((-1 * AlphaModel(scatterer=scatterer, alpha=sixth, **optics).lnlike(params, data), 1e60))
+
+            x = instru.variables.Gaussian(mean=model.scatterer.center[0].guess, std=model.scatterer.center[0].interval/10)
+            y = instru.variables.Gaussian(mean=model.scatterer.center[1].guess, std=model.scatterer.center[1].interval/10)
+            z = instru.variables.Gaussian(mean=model.scatterer.center[2].guess, std=model.scatterer.center[2].interval/10)
+            n = instru.variables.Gaussian(mean=model.scatterer.n.guess, std=model.scatterer.n.interval/10)
+            r = instru.variables.Gaussian(mean=model.scatterer.r.guess, std=model.scatterer.r.interval/10)
+            alpha = instru.variables.Gaussian(mean=model.alpha.guess, std=.3)
+
+            icost = instru.InstrumentedFunction(cost, x, y, z, n, r, alpha)
+            return icost
+        else:
+            return self._make_cost_function(model, data)
+
+    def _cost_function(self, data, scatterer, **kwargs):
+        return self.calc_err_sq(data, scatterer, **kwargs)
+
+    def calc_err_sq(self, data, scatterer, **kwargs):
+        residual = self.calc_residual(data, scatterer, **kwargs)
+        return np.sum(residual ** 2)
+    
+    def calc_residual(self, data, scatterer, **kwargs):
+        dt = data.values.squeeze()
+        scaling = kwargs['alpha']
+        fit = calc_holo(data, scatterer, scaling=scaling).values.squeeze()
+        return fit - dt
+
     def _make_cost_function(self, model, data):
-        return lambda x: model.cost(x, data)
+        return lambda x: -model.lnlike(x, data)
 
     def _get_model_diminsion(self, model):
         return len(model.parameters)
@@ -43,3 +82,16 @@ class NevergradStrategy(HoloPyObject):
 
     def _estimate_error_from_fit(self):
         return 0
+
+    def _scatterer_from_optimizer_params(self, params):
+        try: 
+            x, y, z, index, radius, sixth = params
+        except:
+            return 1.0, 1.0
+        scat_params = {'center.0': x,
+                       'center.1': y,
+                       'center.2': z,
+                       'r': np.min([np.max([1e-16, radius]), 5]),
+                       'n': np.min([np.max([1.00, index]), 2.5])}
+        return scat_params, np.min([np.max([0, sixth]), 1])
+            
--- holopy/inference/tests/test_nevergradfit.py
+++ holopy/inference/tests/test_nevergradfit.py
@@ -4,11 +4,24 @@ import unittest
 
 import numpy as np
 
-from holopy.inference import NevergradStrategy
+
+from holopy.core.process import normalize
+from holopy.core.tests.common import get_example_data
+from holopy.inference.model import AlphaModel
 from holopy.inference.prior import Prior, Uniform
+from holopy.scattering import Sphere, Mie
 from holopy.scattering.scatterer import _expand_parameters
 
+import sys
+sys.path.append('..')
+from nevergradfit import NevergradStrategy
+
 from nevergrad.optimization import optimizerlib
+
+gold_alpha = .6497
+
+gold_sphere = Sphere(1.582+1e-4j, 6.484e-7,
+                     (5.534e-6, 5.792e-6, 1.415e-5))
 
 class TestNevergradStrategy(unittest.TestCase):
     def test_nevergrad_OnePlusOne(self):
@@ -33,6 +46,34 @@ class TestNevergradStrategy(unittest.TestCase):
         result_ok = np.allclose(result.parameters['x'], .5, rtol=.001)
         self.assertTrue(result_ok)
 
+    def test_fit_mie_par_scatterer(self):
+        holo = normalize(get_example_data('image0001'))
+        center_guess = [
+            Uniform(0, 1e-5, name='x', guess=.567e-5),
+            Uniform(0, 1e-5, name='y', guess=.576e-5),
+            Uniform(1e-5, 2e-5, name='z', guess=15e-6),
+            ]
+        scatterer = Sphere(
+            n=Uniform(1, 2, name='n', guess=1.59),
+            r=Uniform(1e-8, 1e-5, name='r', guess=8.5e-7),
+            center=center_guess)
+        alpha = Uniform(0.1, 1, name='alpha', guess=0.6)
+
+        theory = Mie(compute_escat_radial=False)
+        model = AlphaModel(scatterer, theory=theory, alpha=alpha)
+
+        fitter = NevergradStrategy(optimizer='CMA', budget=200)
+        result = fitter.optimize(model, holo)
+        fitted = result.scatterer
+        print(result.parameters)
+        self.assertTrue(np.isclose(fitted.n, gold_sphere.n, rtol=1e-3))
+        self.assertTrue(np.isclose(fitted.r, gold_sphere.r, rtol=1e-3))
+        self.assertTrue(
+            np.allclose(fitted.center, gold_sphere.center, rtol=1e-3))
+        self.assertTrue(
+            np.isclose(result.parameters['alpha'], gold_alpha, rtol=0.1))
+        self.assertEqual(model, result.model)
+
 
 def _simple_cost_function(x, x_obs=None):
     if x_obs is None:
@@ -46,6 +87,9 @@ class _SimpleModel:
 
     def cost(self, x, x_obs=None):
         return _simple_cost_function(x, x_obs)
+
+    def lnlike(self, pars, data):
+        return -self.cost(pars, data)
 
     def _use_parameters(self, parameters, as_attr=True):
         if as_attr:
